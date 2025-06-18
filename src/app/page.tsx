ef6df870be85fcc1f35a891271e9a979296ec491
manhtranduc <manhtranduc0202@gmail.com>
2025-06-18 13:51:28 +0700

    sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,39 @@
+import { AppSidebarClient } from "@/app/_AppSidebarClient";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
 export default function Home() {
-  return <div>hi</div>;
+  return (
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">ManhJobs</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>sdsds</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+
+        <main className="flex-1 ">aaaaaaaaaa</main>
+      </AppSidebarClient>
+    </SidebarProvider>
+  );
 }
+
+//44 15
